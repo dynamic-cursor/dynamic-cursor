e66b9e5f4a64d1d3263e6ea37642e06e498843a9
yxshv <yashverma30115@gmail.com>
2022-10-11 22:06:52 +0530

    testing

--- src/content.tsx
+++ src/content.tsx
@@ -1,6 +1,7 @@
+import { motion } from "framer-motion";
 import React from "react";
 
-export default function Content() {
+export default function Content({ hovering, setHovering }: { hovering: boolean, setHovering: React.Dispatch<React.SetStateAction<boolean>> }) {
 
     const [hover, setHover] = React.useState(false);
 
@@ -9,18 +10,43 @@ export default function Content() {
             <button
                 style={{
                     background: "transparent",
-                    color: "white",
+                    color: hover ? "white" : "white",
                     border: "none",
                     position: "relative",
+                    paddingBottom: "5px",
+                    fontSize: "4rem",
                     isolation: "isolate"
                 }}
-                onMouseOver={() => setHover(true)}
-                onMouseOut={() => setHover(false)}
+                onMouseOver={() => {
+                    setHover(true);
+                    setHovering(true);
+                }}
+                onMouseOut={() => {
+                    setHover(false);
+                    setHovering(false);
+                }}
             >
                 Test
-                {/* {hover && (
-
-                )} */}
+                {hover && (
+                    <motion.div layout layoutId="cursor" style={{
+                        position: "absolute",
+                        left: 0,
+                        right: 0,
+                        bottom: "5px",
+                        width: "100%",
+                        height: "5px",
+                        background: "white",
+                        color: "black",
+                        zIndex: -1,
+                        borderRadius: "2px"
+                    }} 
+                    transition={{
+                        duration: 0.1,
+                        damping: 100,
+                        stiffness: 100
+                    }}
+                />
+            )}
             </button>
         </>
     )
